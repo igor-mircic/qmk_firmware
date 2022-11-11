3a1567847908630cfadaa15660438b30858b9373
Igor <76088411+igor-mircic@users.noreply.github.com>
2022-11-11 23:24:19 +0100

    add more combos

--- keyboards/keychron/k6_pro/ansi/rgb/config.h
+++ keyboards/keychron/k6_pro/ansi/rgb/config.h
@@ -100,4 +100,4 @@
 #endif
 
 
-#define COMBO_COUNT 10
+#define COMBO_COUNT 20
--- keyboards/keychron/k6_pro/ansi/rgb/keymaps/igor-mircic/keymap.c
+++ keyboards/keychron/k6_pro/ansi/rgb/keymaps/igor-mircic/keymap.c
@@ -18,14 +18,14 @@ enum layers{
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 [MAC_BASE] = LAYOUT_ansi_68(
-     KC_GRV,   KC_1,     KC_2,     KC_3,     KC_4,     KC_5,     KC_6,     KC_7,     KC_8,     KC_9,     KC_0,     KC_MINS,  KC_EQL,   KC_BSPC, RGB_MOD,
+     KC_GRV,   KC_1,     KC_2,     KC_3,     KC_4,     KC_5,     KC_6,     KC_7,     KC_8,     KC_9,     KC_0,     KC_MINS,  KC_EQL,   KC_BSPC, KC_DEL,
      KC_TAB,   KC_Q,     KC_W,     KC_E,     KC_R,     KC_T,     KC_Y,     KC_U,     KC_I,     KC_O,     KC_P,     KC_LBRC,  KC_RBRC,  KC_BSLS, KC_HOME,
      KC_ESC,   KC_A,     KC_S,     KC_D,     KC_F,     KC_G,     KC_H,     KC_J,     KC_K,     KC_L,     KC_SCLN,  KC_QUOT,            KC_ENT,  KC_PGUP,
      KC_LSFT,  KC_Z,     KC_X,     KC_C,     KC_V,     KC_B,     KC_N,     KC_M,     KC_COMM,  KC_DOT,   KC_SLSH,  KC_RSFT,  KC_UP,    KC_PGDN,
      KC_LCTL,  KC_LGUI,  KC_LALT,                                KC_SPC,                       KC_RALT, MO(WIN_FN1),MO(FN2), KC_LEFT,  KC_DOWN, KC_RGHT),
 
 [WIN_BASE] = LAYOUT_ansi_68(
-     KC_GRV,   KC_1,     KC_2,     KC_3,     KC_4,     KC_5,     KC_6,     KC_7,     KC_8,     KC_9,     KC_0,     KC_MINS,  KC_EQL,   KC_BSPC, RGB_MOD,
+     KC_GRV,   KC_1,     KC_2,     KC_3,     KC_4,     KC_5,     KC_6,     KC_7,     KC_8,     KC_9,     KC_0,     KC_MINS,  KC_EQL,   KC_BSPC, KC_DEL,
      KC_TAB,   KC_Q,     KC_W,     KC_E,     KC_R,     KC_T,     KC_Y,     KC_U,     KC_I,     KC_O,     KC_P,     KC_LBRC,  KC_RBRC,  KC_BSLS, KC_HOME,
      MO_WNAV,  KC_A,     KC_S,     KC_D,     KC_F,     KC_G,     KC_H,     KC_J,     KC_K,     KC_L,     KC_SCLN,  KC_QUOT,            KC_ENT,  KC_PGUP,
      KC_LSFT,  KC_Z,     KC_X,     KC_C,     KC_V,     KC_B,     KC_N,     KC_M,     KC_COMM,  KC_DOT,   KC_SLSH,  KC_RSFT,  KC_UP,    KC_PGDN,
@@ -66,12 +66,26 @@ const uint16_t PROGMEM jk_combo[] = {KC_J, KC_K, COMBO_END};
 const uint16_t PROGMEM ji_combo[] = {KC_J, KC_I, COMBO_END};
 const uint16_t PROGMEM jo_combo[] = {KC_J, KC_O, COMBO_END};
 const uint16_t PROGMEM km_combo[] = {KC_K, KC_M, COMBO_END};
+const uint16_t PROGMEM kml_combo[] = {KC_K, KC_M, KC_L, COMBO_END};
+const uint16_t PROGMEM kmo_combo[] = {KC_K, KC_M, KC_O, COMBO_END};
+
 const uint16_t PROGMEM df_combo[] = {KC_D, KC_F, COMBO_END};
+const uint16_t PROGMEM fe_combo[] = {KC_F, KC_E, COMBO_END};
+const uint16_t PROGMEM fw_combo[] = {KC_F, KC_W, COMBO_END};
+const uint16_t PROGMEM few_combo[] = {KC_F, KC_E, KC_W, COMBO_END};
+const uint16_t PROGMEM sdc_combo[] = {KC_S, KC_D, KC_C, COMBO_END};
 combo_t key_combos[COMBO_COUNT] = {
     COMBO(lr_shift_combo, KC_CAPS),
     COMBO(jk_combo, KC_ESC),
     COMBO(ji_combo, LCTL(KC_BSPC)),
     COMBO(jo_combo, KC_BSPC),
     COMBO(km_combo, KC_ENTER),
+    COMBO(kml_combo, LALT(KC_ENTER)),
+    COMBO(kmo_combo, LCTL(KC_ENTER)),
+
     COMBO(df_combo, MO(NAV)),
+    COMBO(fe_combo, LCTL(KC_SPC)),
+    COMBO(fw_combo, LCTL(KC_DOT)),
+    COMBO(few_combo, LALT(KC_INS)),
+    COMBO(sdc_combo, KC_DEL),
 };
